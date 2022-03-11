919e8f7ed35d41fb24608c7a4778cb7ac0ef60d9
mbodily <Matt.Bodily@radian.com>
2022-03-11 11:41:57 -0700

    initial setup of classes and controllers

---
+++ ApiProjectMatt/Controllers/ApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ApiProjectMatt.Controllers
+{
+    public class ApiController
+    {
+        [HttpPost]
+        [Route("/request")]
+        public ActionResult PostRequest(string key)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("/callback/{id}")]
+        public ActionResult PostCallback(string id)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.NoContent); ;
+        }
+
+        [HttpPut]
+        [Route("/callback/{id}")]
+        public ActionResult PutCallback(string id)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.NoContent); ;
+        }
+
+        [HttpGet]
+        [Route("/status/{id}")]
+        public ActionResult GetStatus(string id)
+        {
+            return null;
+        }
+    }
+}
